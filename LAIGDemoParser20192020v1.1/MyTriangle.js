41d8a51b21a3894b4284eb5f83147873f4f7bf6f
henrique <up201707046@fe.up.pt>
2019-09-23 15:47:37 +0000

    added triangle primitive and parser

---
+++ LAIGDemoParser20192020v1.1/MyTriangle.js
@@ -0,0 +1,56 @@
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangle extends CGFobject {
+	constructor(scene, id, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+        super(scene);
+        this.x1 = x1;
+        this.y1 = y1;
+        this.z1 = z1;
+        
+        this.x2 = x2;
+        this.y2 = y2;
+        this.z2 = z2;
+        
+        this.x3 = x3;
+        this.y3 = y3;
+        this.z3 = z3;
+        
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			this.x1, this.y1, this.z1,	//0
+			this.x2, this.y2, this.z2,	//1
+			this.x3, this.y3, this.z3	//2
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+            0, 1, 2,
+            1, 0, 2
+        ];
+        
+        //Facing Z positive
+        this.normals = [
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+        ];
+
+        /*
+        Texture coords (s,t)
+        +----------> s
+        |
+        |
+		|
+		v
+        t
+        */
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+    }
+}
