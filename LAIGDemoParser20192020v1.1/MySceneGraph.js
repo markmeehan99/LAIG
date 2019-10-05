6eb0786bed5db0f2f0eea1a9cd9e1244d371c5f4
Mark Meehan <mtimothy38@gmail.com>
2019-10-05 15:18:15 +0100

    fixed compilation error

--- LAIGDemoParser20192020v1.1/MySceneGraph.js
+++ LAIGDemoParser20192020v1.1/MySceneGraph.js
@@ -839,6 +839,6 @@ class MySceneGraph {
         //To test the parsing/creation of the primitives, call the display function directly
 
         this.primitives['cylinder'].display();
-        this.primitives['demoSphere'].display();
+        // this.primitives['sphere'].display();
     }
 }
